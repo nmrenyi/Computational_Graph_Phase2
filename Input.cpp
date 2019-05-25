aa24c72586d0d976644d4ebdca82b36d05e8c8c7
GuangZaiLanYi <jj-li18@mails.tsinghua.edu.cn>
2019-05-25 19:34:30 +0800

    meisha

--- Input.cpp
+++ Input.cpp
@@ -78,9 +78,9 @@ void Inputoperate() {
             new binaryoperation(buffer[2], buffer[4], buffer[3], save);
             save[buffer[0]] = binaryname;
         } else {
-            base* binaryname =
-            new binaryoperation(buffer[2], buffer[4], buffer[3], save);
-            save[buffer[0]] = binaryname;
+            base* bindname =
+            new bindoperation(buffer[3], buffer[4], save);
+            save[buffer[0]] = bindname;
         }
     } else if (buffer.size() == 6) {  // COND
         base* condname =
