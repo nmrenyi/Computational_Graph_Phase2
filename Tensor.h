4617d890345f728a0debbe0b36e181b6d84bc03b
nmrenyi <nmrenyi@outlook.com>
2019-06-09 14:37:10 +0800

    ry

--- Tensor.h
+++ Tensor.h
@@ -1,14 +1,24 @@
 #include <vector>
 #include <map>
 #include <string>
-class Tensor {
- protected:
+
+class BaseTensor {
+ private:
     std::vector<Tensor*> input;
     std::vector<double> data;  // 储存Tensor内部的所有值
     std::vector<int>dim;  // 每一维的大小
+};
+
+class Tensor {
+ protected:
+    BaseTensor baseTensor;
+    bool calculated = 0;
  public:
-    Tensor(){}
-    virtual ~Tensor() {};
+    Tensor();
+    void initialize();  // 使节点都定义为未调用       
+    virtual bool calculate() = 0;  // 是否可算,并且在此过程中进行计算
+    void display();
+    virtual ~Tensor();
 };
 
 class TensorConstant : public Tensor {
@@ -16,42 +26,35 @@ class TensorConstant : public Tensor {
     const std::vector<double> data;  // 储存Tensor内部的所有值
     const std::vector<int>dim;  // 每一维的大小
  public:
-    TensorConstant(std::vector<int>dimInfo, std::vector<double>dataInfo) : dim(dimInfo), data(dataInfo) {}
-    
+    TensorConstant(std::vector<int>dimInfo, std::vector<double>dataInfo);
+    bool calculate();
 };
 
 class TensorPlaceholder : public Tensor {
  public:
-    TensorPlaceholder(std::vector<int>dimInfo) {
-        dim = dimInfo;
-    }
+    TensorPlaceholder(std::vector<int>dimInfo);
+    bool calculate();
 };
 
 class TensorVariable : public Tensor {
  public:
-    TensorVariable(std::vector<int>dimInfo, std::vector<double>dataInfo) {
-        dim = dimInfo;
-        data = dataInfo;
-    }
+    TensorVariable(std::vector<int>dimInfo, std::vector<double>dataInfo);
+    bool calculate();
 };
 
 class TensorSingleOperation : public Tensor {
  private:
     std::string operationName;
  public:
-    TensorSingleOperation(std::string target, std::string operatorName, std::map<std::string, Tensor*>& save) {
-        operationName = operatorName;
-        input.push_back(save[target]);
-    }
+    TensorSingleOperation(std::string target, std::string operatorName, std::map<std::string, Tensor*>& save);
+    bool calculate();
 };
 
 class TensorBinaryOperation : public Tensor {
  private:
     std::string operationName;
  public:
-    TensorBinaryOperation(std::string target1, std::string target2, std::string operatorName, std::map<std::string, Tensor*>& save) {
-        operationName = operatorName;
-        input.push_back(save[target1]);
-        input.push_back(save[target2]);
-    }
+    TensorBinaryOperation(std::string target1, std::string target2, std::string operatorName, std::map<std::string, Tensor*>& save);
+    bool calculate();
+
 };
